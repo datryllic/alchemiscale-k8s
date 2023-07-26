b92fdd537e80beb658755b77e3661222730f9187
David Dotson <dotsdl@gmail.com>
2023-07-26 08:58:21 -0700

    Renames and namespacing of all resources

--- server/secrets.sh
+++ server/secrets.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-kubectl delete secret neo4j-secrets 
-kubectl delete secret aws-secrets 
-kubectl delete secret jwt-secrets 
+kubectl -n alchemiscale delete secret alchemiscale-neo4j-secrets 
+kubectl -n alchemiscale delete secret alchemiscale-aws-secrets 
+kubectl -n alchemiscale delete secret alchemiscale-jwt-secrets 
 
 # neo4j
 USER=""
 PASS=""
-kubectl create secret generic neo4j-secrets --from-literal="NEO4J_USER=$USER"\
-                                            --from-literal="NEO4J_PASS=$PASS"\
-                                            --from-literal="NEO4J_AUTH=$USER/$PASS"
+kubectl -n alchemiscale create secret generic alchemiscale-neo4j-secrets --from-literal="NEO4J_USER=$USER"\
+                                                         --from-literal="NEO4J_PASS=$PASS"\
+                                                         --from-literal="NEO4J_AUTH=$USER/$PASS"
 
 # aws
-kubectl create secret generic aws-secrets --from-literal="AWS_ACCESS_KEY_ID=" \
-                                          --from-literal="AWS_SECRET_ACCESS_KEY=" \
-                                          --from-literal="AWS_S3_BUCKET=" \
-                                          --from-literal="AWS_S3_PREFIX=" \
-                                          --from-literal="AWS_DEFAULT_REGION="
+kubectl -n alchemiscale create secret generic alchemiscale-aws-secrets --from-literal="AWS_ACCESS_KEY_ID=" \
+                                                       --from-literal="AWS_SECRET_ACCESS_KEY=" \
+                                                       --from-literal="AWS_S3_BUCKET=" \
+                                                       --from-literal="AWS_S3_PREFIX=" \
+                                                       --from-literal="AWS_DEFAULT_REGION="
 # jwt
-kubectl create secret generic jwt-secrets --from-literal="JWT_SECRET_KEY="
+kubectl -n alchemiscale create secret generic alchemiscale-jwt-secrets --from-literal="JWT_SECRET_KEY="
